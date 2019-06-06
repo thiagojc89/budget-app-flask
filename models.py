6bf4b9b47b30b007250c04d603aa4fb4ea04b3bc
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-06-06 10:48:50 -0500

    deploy

--- models.py
+++ models.py
@@ -12,69 +12,69 @@ from playhouse.db_url import connect
 
 
 if 'ON_HEROKU' in os.environ:
-    print('this is the databe I\'m going to connect.  >>>>>', os.environ.get('DATABASE_URL') )
+	print('this is the databe I\'m going to connect.  >>>>>', os.environ.get('DATABASE_URL') )
 	DATABASE = connect(os.environ.get('DATABASE_URL'))
 else:
-    DATABASE = SqliteDatabase('budgetapp.sqlite')
+	DATABASE = SqliteDatabase('budgetapp.sqlite')
 
 class User(UserMixin, Model): 
-    id              = PrimaryKeyField(null=False)
-    first_name      = CharField()
-    last_name       = CharField()
-    email           = CharField()
-    password        = CharField()
-    balance			= CharField(default='00,00')
-    created_at		= DateTimeField(default=datetime.datetime.now)
+	id              = PrimaryKeyField(null=False)
+	first_name      = CharField()
+	last_name       = CharField()
+	email           = CharField()
+	password        = CharField()
+	balance			= CharField(default='00,00')
+	created_at		= DateTimeField(default=datetime.datetime.now)
 
-    class Meta:
-        database = DATABASE
+	class Meta:
+		database = DATABASE
 
-    @classmethod
-    def create_user(cls, first_name, last_name, email, password, **kwargs):
-        try:
-            cls.select().where(
-                (cls.email==email)
-                ).get()
-        except cls.DoesNotExist:
-        	print('>>>>>>>> going to create new user <<<<<<<<<<')
-        	user = cls(first_name=first_name, last_name=last_name, email=email, password=password)
-        	user.password = generate_password_hash(password)
-        	user.save()
-        	return user
-        else:
-        	raise Exception("User with that email address already exists")
+	@classmethod
+	def create_user(cls, first_name, last_name, email, password, **kwargs):
+		try:
+			cls.select().where(
+				(cls.email==email)
+				).get()
+		except cls.DoesNotExist:
+			print('>>>>>>>> going to create new user <<<<<<<<<<')
+			user = cls(first_name=first_name, last_name=last_name, email=email, password=password)
+			user.password = generate_password_hash(password)
+			user.save()
+			return user
+		else:
+			raise Exception("User with that email address already exists")
 
 
 
 class Budget(Model):
-    id              = PrimaryKeyField(null=False) 
-    user_id			= ForeignKeyField(User, related_name='user')
-    name            = CharField()
-    start_date		= CharField()
-    end_date		= CharField()
-    created_at      = DateTimeField(default=datetime.datetime.now)
+	id              = PrimaryKeyField(null=False) 
+	user_id			= ForeignKeyField(User, related_name='user')
+	name            = CharField()
+	start_date		= CharField()
+	end_date		= CharField()
+	created_at      = DateTimeField(default=datetime.datetime.now)
 
-    class Meta:
-        database = DATABASE
-    
+	class Meta:
+		database = DATABASE
+	
 
 class Item(Model):
-    id              = PrimaryKeyField(null=False)
-    user_id         = ForeignKeyField(User, related_name='user')
-    budget_id       = ForeignKeyField(Budget, related_name='budget')
-    name 			= CharField()
-    value			= CharField()
-    due_date		= CharField()
-    payment_date	= CharField()
-    transaction		= CharField()
-    created_at      = DateTimeField(default=datetime.datetime.now)
+	id              = PrimaryKeyField(null=False)
+	user_id         = ForeignKeyField(User, related_name='user')
+	budget_id       = ForeignKeyField(Budget, related_name='budget')
+	name 			= CharField()
+	value			= CharField()
+	due_date		= CharField()
+	payment_date	= CharField()
+	transaction		= CharField()
+	created_at      = DateTimeField(default=datetime.datetime.now)
 
-    class Meta:
-        database = DATABASE
+	class Meta:
+		database = DATABASE
   
 
 def initialize(): 
-    print('================creating my tables ================')
-    DATABASE.connect()
-    DATABASE.create_tables([User, Budget, Item], safe=True)
-    DATABASE.close()
+	print('================creating my tables ================')
+	DATABASE.connect()
+	DATABASE.create_tables([User, Budget, Item], safe=True)
+	DATABASE.close()
