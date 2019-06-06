b34865bd16e91f28edb337e71bf355326e653d11
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-06-06 09:24:30 -0500

    trying to deploy

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, g
 from flask_login import LoginManager, current_user
 from flask_cors import CORS
-# import config
+import config
 import models
 from resources.auth import auth_api
 from resources.user import user_api
 import os
 
-app = Flask(__name__, instance_relative_config=True)
-# with app.app_context():
-	# init_db()
+app = Flask(__name__)
+
 
 app.config.from_pyfile('config.py')
 
@@ -17,13 +16,8 @@ app.config.from_pyfile('config.py')
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# print('======================')
-# print(app.config)
 
-if 'ON_HEROKU' in os.environ:
-	app.secret_key = SECRET_KEY
-else:
-	app.secret_key = app.config['SECRET_KEY']
+app.secret_key = app.config['SECRET_KEY']
 
 
 
@@ -35,8 +29,8 @@ def load_user(userid):
         return None
 
 
-CORS(auth_api, origins=['http://localhost:3000', 'https://moneychart.herokuapp.com'], supports_credentials=True)
-CORS(user_api, origins=['http://localhost:3000', 'https://moneychart.herokuapp.com'], supports_credentials=True)
+CORS(auth_api, origins=["http://localhost:3000", "https://moneychart.herokuapp.com"], supports_credentials=True)
+CORS(user_api, origins=["http://localhost:3000", "https://moneychart.herokuapp.com"], supports_credentials=True)
 
 
 app.register_blueprint(auth_api, url_prefix='/api/v1/auth')
